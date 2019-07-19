d4e1f6810ce8476c0bd4c23c06f1028975a246e7
n.ermolenko <n.ermolenko@rt-solar.ru>
2019-07-19 17:15:31 +0300

    custom scroll

--- rn/app/view/workspace/SubMenu.js
+++ rn/app/view/workspace/SubMenu.js
@@ -9,24 +9,11 @@ Ext.define('RN.view.workspace.SubMenu', {
 
     reference: 'workspace-sub-menu',
 
-    controller: {
+    requires: [
+        'RN.view.workspace.SubMenuController'
+    ],
 
-        bindings: {
-            onChangeA: '{subMenuState}'
-        },
-
-        onChangeA(subMenuState) {
-            const subMenu = this.getView();
-            if (subMenuState) {
-                subMenu.getHideAnimation().stop();
-                subMenu.show();
-            }
-            else {
-                subMenu.getShowAnimation().stop();
-                subMenu.hide();
-            }
-        }
-    },
+    controller: 'rn-workspace-sub-menu',
 
     layout: 'fit',
     width: '80%',
@@ -47,14 +34,133 @@ Ext.define('RN.view.workspace.SubMenu', {
         direction: 'left'
     },
 
-    layout: 'fit',
+    layout: 'vbox',
 
     items: {
         xtype: 'panel',
         title: 'Меню',
+        flex: 1,
+        layout: 'vbox',
         iconCls: `${Ext.baseCSSPrefix}fa fa-wrench`,
-        items: {
-            xtype: 'rn-workspace-settings'
-        }
+        items: [
+            {
+                xtype: 'rn-workspace-settings'
+            },
+            {
+                xtype: 'container',
+                layout: 'hbox',
+                flex: 1,
+                items: [
+                    {
+                        xtype: 'toolbar',
+                        docked: 'top',
+                        items: [
+                            {
+                                text: 'ADD',
+                                handler(btn) {
+                                    btn.up().up().down('list').getStore().add({id:Math.random(),name:'dfgdgdfgfg - ' + Math.random()})
+                                }
+                            }
+                        ]
+                    },
+                    {
+                        xtype: 'list',
+                        reference: 'scrolledList',
+                        flex: 1,
+                        itemTpl: '{name}',
+                        data: (function () {
+                            const res = [];
+                            for (let i = 0, l = 77; i < 77; i++) {
+                                res.push({
+                                    id: i + 1,
+                                    name: 'List Item # ' + (i + 1)
+                                })
+                            }
+                            return res;
+                        })()
+                    },
+                    {
+                        width: 12,
+                        listeners: {
+                            resize() {
+                                console.log(66, arguments);
+                                
+
+                            }
+                        },
+                        items: {
+                            width: 12,
+                            height: 40,
+                            minHeight: 40,
+                            style: {
+                                width: 10,
+                                height: 40,
+                                background: 'rgb(40, 130, 205)',
+                                borderRadius: '0px'
+                            },
+                            listeners: {
+                                desroy() {
+                                    console.log('desroy');
+                                    
+                                },
+                                painted(track) {
+                                    const list = track.up().up().down('list'),
+                                        scroller = list.getScrollable(),
+                                        trackHeight = track.up().innerElement.getHeight(),
+                                        barHeight = track.getHeight();
+
+
+                                        track.el.setStyle('top', `0px`)
+                                        // console.log(9999,scroller.position.y);
+                                        if(track.customScrolled) {
+                                            return;
+                                        }
+                                        track.customScrolled = true;
+                                        scroller.on({
+                                            scroll() {
+                                                track.el.setStyle('top', `${(trackHeight - barHeight) * (scroller.position.y/ scroller.getMaxUserPosition().y)}px`)
+                                            // console.log(444, scroller.position.y/ scroller.getMaxUserPosition().y);
+
+                                        }
+                                    });
+                      
+                                    // track.el.setStyle('top', `${(trackHeight - barHeight) * (scroller.position.y/ scroller.getMaxUserPosition().y)}px`)
+                                    // console.log(9999,scroller.position.y, barHeight, trackHeight, scroller.getMaxUserPosition().y);
+
+                                    new Ext.drag.Source({
+                                        element: track.el,
+                                        constrain: {
+                                            element: true,
+                                            vertical: true
+                                        },
+                                        listeners: {
+                                            dragend: {
+                                                // buffer: 40,
+                                                fn(bar, info) {
+                                                console.log('dragend');
+                                                
+                                                    const list = track.up().up().down('list'),
+                                                        scroller = list.getScrollable(),
+                                                        trackHeight = track.up().innerElement.getHeight(),
+                                                        barHeight = track.innerElement.getHeight(),
+                                                        posY = parseInt(bar.getElement().getStyleValue('top')),
+                                                        ratio = posY / (trackHeight - barHeight);
+
+                                                    scroller.doScrollTo(0, scroller.getMaxUserPosition().y * ratio);
+
+
+
+                                                    // console.log(22, scroller.getMaxUserPosition(), ratio, barHeight, trackHeight, parseInt(bar.getElement().getStyleValue('top')));
+                                                }
+                                            }
+                                        }
+                                    });
+                                }
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
     }
 });
